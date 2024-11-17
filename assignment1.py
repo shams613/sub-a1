ec6412cf42d312977b269ff5fe9e526988800057
SHAMSBIN HARUN <sbharun@myseneca.ca>
2024-11-16 23:45:51 -0500

    Completed Assignment 1 version C

--- assignment1.py
+++ assignment1.py
@@ -1,101 +1,116 @@
 #!/usr/bin/env python3
 
-'''
-OPS445 Assignment 1
+"""
+OPS445 Assignment 1 Version C
 Program: assignment1.py 
 The python code in this file is original work written by
-"Student Name". No code in this file is copied from any other source
+Shams Bin Harun. No code in this file is copied from any other source
 except those provided by the course instructor, including any person,
-textbook, or on-line resource. I have not shared this python script
+textbook, or online resource. I have not shared this Python script
 with anyone or anything except for submission for grading. I understand
 that the Academic Honesty Policy will be enforced and
 violators will be reported and appropriate action will be taken.
 
-Author: <Student Name>
-Semester: <Fall/Winter/Summer> <Year>
-Description: <fill this in>
-'''
+Author: Shams Bin Harun
+Student ID: 155871221
+Semester: Fall 2024
+Description: This script calculates the end date, including the day of the week,
+             given a start date and a number of days.
+"""
 
 import sys
 
 def day_of_week(date: str) -> str:
-    "Based on the algorithm by Tomohiko Sakamoto"
+    """ Returns the day of the week for a given date in DD/MM/YYYY format. Based on the algorithm by Tomohiko Sakamoto. """
     day, month, year = (int(x) for x in date.split('/'))
-    days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'] 
-    offset = {1:0, 2:3, 3:2, 4:5, 5:0, 6:3, 7:5, 8:1, 9:4, 10:6, 11:2, 12:4}
+    days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+    offset = {1: 0, 2: 3, 3: 2, 4: 5, 5: 0, 6: 3, 7: 5, 8: 1, 9: 4, 10: 6, 11: 2, 12: 4}
     if month < 3:
         year -= 1
-    num = (year + year//4 - year//100 + year//400 + offset[month] + day) % 7
+    num = (year + year // 4 - year // 100 + year // 400 + offset[month] + day) % 7
     return days[num]
 
 def leap_year(year: int) -> bool:
-    "return true if the year is a leap year"
-    ...
+    """  Returns True if the year is a leap year, otherwise False. """
+    return (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0)
 
-def mon_max(month:int, year:int) -> int:
-    "returns the maximum day for a given month. Includes leap year check"
-    ...
+def mon_max(month: int, year: int) -> int:
+    """ Returns the maximum number of days for the given month and year. """
+    if month == 2:  # February
+        return 29 if leap_year(year) else 28
+    elif month in [4, 6, 9, 11]:  # April, June, September, November
+        return 30
+    else:
+        return 31  # All other months
 
-def after(date: str) -> str: 
-    '''
-    after() -> date for next day in DD/MM/YYYY string format
-
-    Return the date for the next day of the given date in DD/MM/YYYY format.
-    This function has been tested to work for year after 1582
-    '''
+def after(date: str) -> str:
+    """ Returns the next day's date in DD/MM/YYYY format. """
     day, mon, year = (int(x) for x in date.split('/'))
-    day += 1  # next day
-
-    lyear = year % 4
-    if lyear == 0:
-        leap_flag = True
-    else:
-        leap_flag = False  # this is not a leap year
-
-    lyear = year % 100
-    if lyear == 0:
-        leap_flag = False  # this is not a leap year
-
-    lyear = year % 400
-    if lyear == 0:
-        leap_flag = True  # this is a leap year
-    
-    mon_dict= {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30,
-           7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
-    if mon == 2 and leap_flag:
-        mon_max = 29
-    else:
-        mon_max = mon_dict[mon]
-    
-    if day > mon_max:
+    day += 1
+    if day > mon_max(mon, year):
+        day = 1
         mon += 1
         if mon > 12:
-            year += 1
             mon = 1
-        day = 1  # if tmp_day > this month's max, reset to 1 
+            year += 1
     return f"{day:02}/{mon:02}/{year}"
 
 def before(date: str) -> str:
-    "Returns previous day's date as DD/MM/YYYY"
-    ...
+    """  Returns the previous day's date in DD/MM/YYYY format. """
+    day, mon, year = (int(x) for x in date.split('/'))
+    day -= 1
+    if day < 1:
+        mon -= 1
+        if mon < 1:
+            mon = 12
+            year -= 1
+        day = mon_max(mon, year)
+    return f"{day:02}/{mon:02}/{year}"
 
 def usage():
-    "Print a usage message to the user"
+    """ Prints a usage message and exits the script. """
     print("Usage: " + str(sys.argv[0]) + " DD/MM/YYYY NN")
     sys.exit()
 
 def valid_date(date: str) -> bool:
-    "check validity of date"
-    ...
+    """ Validates if the given date is in DD/MM/YYYY format and is a real date. """
+    try:
+        day, month, year = (int(x) for x in date.split('/'))
+        if 1 <= month <= 12 and 1 <= day <= mon_max(month, year):
+            return True
+        return False
+    except (ValueError, IndexError):
+        return False
 
 def day_iter(start_date: str, num: int) -> str:
-    "iterates from start date by num to return end date in DD/MM/YYYY"
-    ...
+    """ Iterates from a start date by the given number of days. Returns the resulting date in DD/MM/YYYY format. """
+    current_date = start_date
+    if num > 0:
+        for _ in range(num):
+            current_date = after(current_date)
+    elif num < 0:
+        for _ in range(-num):
+            current_date = before(current_date)
+    return current_date
 
 if __name__ == "__main__":
-    # check length of arguments
-    # check first arg is a valid date
-    # check that second arg is a valid number (+/-)
-    # call day_iter function to get end date, save to x
-    # print(f'The end date is {day_of_week(x)}, {x}.')
-    pass
+    """ Checks the number of command-line arguments """
+    if len(sys.argv) != 3:
+        usage()
+
+    start_date = sys.argv[1]
+    try:
+        num_days = int(sys.argv[2])
+    except ValueError:
+        usage()
+
+    """ Validate the input date """
+    if not valid_date(start_date):
+        usage()
+
+    """ Calculate the end date """
+    end_date = day_iter(start_date, num_days)
+    end_day = day_of_week(end_date)
+
+    """ Prints the result """
+    print(f"The end date is {end_day}, {end_date}.")
